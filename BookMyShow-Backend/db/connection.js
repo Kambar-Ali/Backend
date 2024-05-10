3755dbb07065f81a28cf2d34ab5884f01d18a73f
Kambar Ali <alikambar9125@gmail.com>
2024-05-10 10:11:02 +0530

    Backend

---
+++ BookMyShow-Backend/db/connection.js
@@ -0,0 +1,22 @@
+//connect the MongoClient require mongodb -------------->
+const { MongoClient } = require("mongodb");
+// Connecting to database using connection string and speciying if there is any error or it was successfull
+  // mongoose
+
+const mongoose=require("mongoose");
+
+const dbUrl = process.env.MONGODB_URL//Please Copy and paste the address of Majority
+const connectionParams ={
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+}
+// for development use this
+const connectToMongo = async () => {
+mongoose.connect(dbUrl, connectionParams).then(()=>{
+  console.info("Connection on the DB")
+}).catch((e)=>{
+  console.log("Error:", e);
+});
+}
+
+exports.connection = connectToMongo;
